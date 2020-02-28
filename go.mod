87564c1be8b52a859a6b4639c6d37b4ee2e7c974
phil <philcheng@tencent.com>
2020-02-28 10:46:31 +0800

    v2.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/chengdanhao/testmod
+module github.com/chengdanhao/testmod/v2
 
 go 1.13
--- testmod.go
+++ testmod.go
@@ -3,3 +3,10 @@ import "fmt"
 func SayHello(name string) string {
    return fmt.Sprintf("你好 %s", name)
 }
+package testmod
+import (
+"fmt"
+)
+func SayHello(name, str string) string {
+return fmt.Sprintf("你好, %s, %s", name, str)
+}
